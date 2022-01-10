29d026667f841a59cb44d3c4ad2e5a2aac6f1723
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2022-01-10 19:25:41 +0800

    Database established

---
+++ models/movie.model.js
@@ -0,0 +1,61 @@
+const { DataTypes, Model } = require("sequelize");
+
+module.exports = function (sequelize) {
+  class Movie extends Model {}
+
+  Movie.init(
+    {
+      movie_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: "movie_id",
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "title",
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "image",
+      },
+      genre: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "genre",
+      },
+      synopsis: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "synopsis",
+      },
+      release_date: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "release_date",
+      },
+      cast_members: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "cast_members",
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+      },
+    },
+    {
+      sequelize,
+      modelName: "Movie",
+      tableName: "movies",
+    }
+  );
+
+  return Movie;
+};
