57b6d2271e152233dd589d037131f2a750f331f5
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2022-01-13 23:15:38 +0800

    Deployed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require('./routes');
 
-
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port 3000 ...");
+//Automatically sets port number
+app.listen(process.env.PORT, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
